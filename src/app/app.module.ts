87576442688d03b489162111c8648f1f3dd470d9
Maysara-Nasser <85829180+Maysara-Nasser@users.noreply.github.com>
2023-05-30 16:51:01 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FlightResultComponent } from './components/flight-result/flight-result.component';
+import { SelectedFlightComponent } from './bonus/selected-flight/selected-flight.component';
+import { FilterComponent } from './components/flight-result/filter/filter.component';
+import { FlightCardComponent } from './components/flight-result/flight-card/flight-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FlightResultComponent,
+    SelectedFlightComponent,
+    FilterComponent,
+    FlightCardComponent
   ],
   imports: [
     BrowserModule,
